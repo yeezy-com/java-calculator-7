6fc2d96daa56d09135e90e39c792b5211b5a4cac
GiSeok Kang <ime.rokz@gmail.com>
2025-10-16 20:42:32 +0900

    refactor: 문자열 분리 로직 메서드 분리

--- src/test/java/calculator/Splitter.java
+++ src/test/java/calculator/Splitter.java
@@ -6,6 +6,20 @@ import java.util.List;
 public class Splitter {
 
     public String[] split(final String input) {
+        List<String> strings = splitOfColonAndComma(input);
+        String[] ans = getArrayFrom(strings);
+        return ans;
+    }
+
+    private String[] getArrayFrom(List<String> strings) {
+        String[] ans = new String[strings.size()];
+        for (int i = 0; i < strings.size(); i++) {
+            ans[i] = strings.get(i);
+        }
+        return ans;
+    }
+
+    private List<String> splitOfColonAndComma(String input) {
         List<String> strings = new ArrayList<>();
 
         String[] split = input.split(",");
@@ -14,11 +28,6 @@ public class Splitter {
                 strings.add(tmp);
             }
         }
-
-        String[] ans = new String[strings.size()];
-        for (int i = 0; i < strings.size(); i++) {
-            ans[i] = strings.get(i);
-        }
-        return ans;
+        return strings;
     }
 }
