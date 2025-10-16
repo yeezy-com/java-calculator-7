a35852373b2ae7a563f566af25e35e2e7dbb897e
GiSeok Kang <ime.rokz@gmail.com>
2025-10-16 20:18:18 +0900

    red: 콜론으로 된 문자열 구분할 수 있음을 테스트

--- src/test/java/calculator/SplitterTest.java
+++ src/test/java/calculator/SplitterTest.java
@@ -31,4 +31,16 @@ public class SplitterTest {
         assertThat(output).hasSize(4);
         assertThat(output).contains(expected);
     }
+
+    @Test
+    void 콜론으로_된_문자열을_구분할_수_있다() {
+        Splitter splitter = new Splitter();
+        String input = "f:3:2";
+
+        var output = splitter.split(input);
+
+        String[] expected = input.split(":");
+        assertThat(output).hasSize(3);
+        assertThat(output).contains(expected);
+    }
 }
