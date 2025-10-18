1b2d7b218f526c80c2e77d8884f22a1a709de85d
GiSeok Kang <ime.rokz@gmail.com>
2025-10-19 00:31:44 +0900

    red: 구분한 숫자들의 합 테스트 작성 2

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -15,4 +15,14 @@ public class CalculatorTest {
 
         assertThat(sum).isEqualTo(9);
     }
+
+    @Test
+    void 구분한_문자열의_합을_반환한다2() {
+        var calculator = new Calculator(new Splitter());
+        var input = "4,4,2,2";
+
+        var sum = calculator.sum(input);
+
+        assertThat(sum).isEqualTo(12);
+    }
 }
