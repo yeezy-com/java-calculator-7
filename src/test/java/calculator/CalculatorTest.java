de26a34cfb3d8fe41ac5cc4a294a800e9710d398
GiSeok Kang <ime.rokz@gmail.com>
2025-10-19 00:30:06 +0900

    red: 구분한 숫자들의 합 테스트 작성

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void 구분한_문자열의_합을_반환한다() {
+        var calculator = new Calculator(new Splitter());
+        var input = "4:3:2";
+
+        var sum = calculator.sum(input);
+
+        assertThat(sum).isEqualTo(9);
+    }
+}
