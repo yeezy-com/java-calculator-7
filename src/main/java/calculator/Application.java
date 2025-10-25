73669aa7be8af0bd795ca3955b98b281f60d6248
GiSeok Kang <ime.rokz@gmail.com>
2025-10-25 19:39:48 +0900

    refactor: input 출력문 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,7 +13,6 @@ public class Application {
 
             System.out.println("덧셈할 문자열을 입력해 주세요.");
             String s = bufferedReader.readLine();
-            System.out.println("input: " + s);
 
             long sum = calculator.sum(s);
 
